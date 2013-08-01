c151d406ef8d1ee72fce912b3d8289cb34265eec
Bertrand Gressier <bertrand.gressier@adeoservices.com>
2013-08-02 00:15:26 +0200

    save token in localStorage

--- app/scripts/filters/bytesToMo.js
+++ app/scripts/filters/bytesToMo.js
@@ -1,14 +1,17 @@
 'use strict';
 
-angular.module('dlplateformApp').filter('bytesToMo', function(){
-    return function(input){
-        var fileSize = Math.round(parseFloat(input)/1000000);
-        if(fileSize<1000)return fileSize+' Mo';
-        else return Math.round(fileSize/10)/100+' Go';
+angular.module('dlplateformApp').filter('bytesToMo', function () {
+    return function (input) {
+        var fileSize = Math.round(parseFloat(input) / 1000000);
+        if (fileSize < 1000) {
+            return fileSize + ' Mo';
+        } else {
+            return Math.round(fileSize / 10) / 100 + ' Go';
+        }
 
 
         //return parseFloat(input/1000000);
-    }
+    };
 });
 
 
--- app/scripts/services/t411/app.js
+++ app/scripts/services/t411/app.js
@@ -1,3 +1,8 @@
 'use strict';
 
-angular.module('t411service',['config']);
+angular.module('t411service',['config']).run(function($http){
+    //reload token
+    if (localStorage.t411token != null) {
+        $http.defaults.headers.common.Authorization = localStorage.t411token;
+    }
+});
