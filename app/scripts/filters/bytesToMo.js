35e0c667e49b160befe3cb3643b2847b31985a2f
Gregory Douay <gregory.douay@gmail.com>
2013-08-02 00:16:50 +0200

    Add files size support Add filter 'bytesToMo' : convert size to Mo or Go

--- app/scripts/filters/bytesToMo.js
+++ app/scripts/filters/bytesToMo.js
@@ -5,12 +5,7 @@ angular.module('dlplateformApp').filter('bytesToMo', function(){
         var fileSize = Math.round(parseFloat(input)/1000000);
         if(fileSize<1000)return fileSize+' Mo';
         else return Math.round(fileSize/10)/100+' Go';
-
-
-        //return parseFloat(input/1000000);
     }
 });
 
-
-//return parseFloat(input)/8000000;
 
