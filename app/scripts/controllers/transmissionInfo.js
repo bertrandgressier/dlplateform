f5b24ed19e2cad9e79a8389edc0554dd0b53e12e
Bertrand Gressier <bertrand.gressier@adeoservices.com>
2013-08-14 00:02:19 +0200

    play with progress bar

--- app/scripts/controllers/transmissionInfo.js
+++ app/scripts/controllers/transmissionInfo.js
@@ -2,13 +2,19 @@
 
 angular.module('dlplateformApp').controller('TransmissionInfoCtrl', function ($scope, Transmission, $timeout) {
 
+    $scope.refresh = true;
+
     function refresh() {
+
+        if (!$scope.refresh) {
+            return;
+        }
 
         Transmission.getTorrents().then(function (torrents) {
             return torrents.data['arguments'].torrents;
         }).then(function (torrents) {
                 $scope.torrents = torrents;
-        });
+            });
 
         $scope.info = Transmission.stats().then(function (stats) {
             return stats.data['arguments'];
@@ -20,6 +26,12 @@ angular.module('dlplateformApp').controller('TransmissionInfoCtrl', function ($s
     }
 
     refresh();
+
+    $scope.$watch('refresh', function () {
+        if ($scope.refresh) {
+            refresh();
+        }
+    });
 
 
 });
